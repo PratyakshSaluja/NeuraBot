caa6915c09bcd2473fb3cacc676f6ef5976e12df
PratyakshSaluja <pratyakshsirsa@gmail.com>
2024-09-08 15:38:54 +0530

    sd

--- nullPointers_2.0/scripts/Oauth.js
+++ nullPointers_2.0/scripts/Oauth.js
@@ -1,1 +1,56 @@
-  
+// // oauth.js
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const FacebookStrategy = require('passport-facebook').Strategy;
+// const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+
+// // Serialize and deserialize user
+// passport.serializeUser((user, done) => {
+//     done(null, user);
+// });
+// passport.deserializeUser((obj, done) => {
+//     done(null, obj);
+// });
+
+// // Google OAuth Strategy
+// passport.use(new GoogleStrategy({
+//     clientID: 'apps.googleusercontent.com',
+//     clientSecret: '',
+//     callbackURL: '/auth/google/callback'
+// }, (accessToken, refreshToken, profile, done) => {
+//     return done(null, profile);
+// }));
+
+// // Facebook OAuth Strategy
+// passport.use(new FacebookStrategy({
+//     clientID: 'FACEBOOK_APP_ID',
+//     clientSecret: 'FACEBOOK_APP_SECRET',
+//     callbackURL: '/auth/facebook/callback',
+//     profileFields: ['id', 'displayName', 'email']
+// }, (accessToken, refreshToken, profile, done) => {
+//     return done(null, profile);
+// }));
+
+// // LinkedIn OAuth Strategy
+// passport.use(new LinkedInStrategy({
+//     clientID: 'LINKEDIN_CLIENT_ID',
+//     clientSecret: 'LINKEDIN_CLIENT_SECRET',
+//     callbackURL: '/auth/linkedin/callback',
+//     scope: ['r_emailaddress', 'r_liteprofile']
+// }, (accessToken, refreshToken, profile, done) => {
+//     return done(null, profile);
+// }));
+
+// // Middleware to protect routes
+// function ensureAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         return next();
+//     }
+//     res.redirect('/login');
+// }
+
+// // Export routes and strategies
+// module.exports = {
+//     passport,
+//     ensureAuthenticated
+// };
